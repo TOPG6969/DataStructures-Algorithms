080f3492f529ef0e2c0f5e522aa5b4ad8cf63a33
Harshit Shukla <120409631+Harshitshukla0208@users.noreply.github.com>
2023-07-04 18:15:53 +0530

    Create Count of a particular element in a sorted array.cpp

---
+++ Algorithms/Binary Search/Count of a particular element in a sorted array.cpp
@@ -0,0 +1,51 @@
+
+// TC - O(nlogn), SC - O(1)
+
+#include <iostream>
+using namespace std;
+
+long long firstOccurance(vector<long long>& v, long long target)
+{
+    int n = v.size();
+    int start = 0 , end = n - 1 , ans = -1;
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+        if (target == v[mid])
+        {
+            ans = mid;
+            end = mid - 1;
+        }
+        else if (target > v[mid])
+             start = mid + 1;
+        
+        else
+            end = mid - 1;   
+    }
+    return ans;
+}
+long long lastOcc(vector<long long>& v, long long target)
+{
+    int n = v.size();
+    int start = 0 , end = n - 1 , ans = -1;
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+        if (target == v[mid])
+        {
+            ans = mid;
+            start = mid + 1;
+        }
+        else if (target > v[mid])
+            start = mid + 1;
+        
+        else
+            end = mid - 1;
+    }
+    return ans;
+}
+int count(vector<long long> v, long long target)
+{
+        int count = lastocc(v,target)-firstocc(v,target);
+        return count;
+}
