24cd53b10b88e746c150567a7acb52b66773a713
Harshit Shukla <120409631+Harshitshukla0208@users.noreply.github.com>
2023-07-04 18:02:47 +0530

    Update NormalBinarySearchCode.cpp

--- Algorithms/Binary Search/NormalBinarySearchCode.cpp
+++ Algorithms/Binary Search/NormalBinarySearchCode.cpp
@@ -1,14 +1,16 @@
+// TC - O(logn)  ,  SC - O(1)
+
 #include <iostream>
 using namespace std;
 
-bool BinarySearch(int *nums, int target, int size)
+int BinarySearch(vector<int>& nums, int target)
 {
     int start = 0, end = size - 1;
     while (start <= end)
     {
         int mid = start + (end - start) / 2;
         if (nums[mid] == target)
-            return 1;
+            return mid;
 
         else if (nums[mid] < target)
             start = mid + 1;
@@ -16,20 +18,5 @@ bool BinarySearch(int *nums, int target, int size)
         else
             end = mid - 1;
     }
-    return 0;
-}
-int main()
-{
-    int nums[5] = {91, 92, 93, 94, 95};
-    int target;
-    cout << "enter target to check the presence" << endl;
-    cin >> target;
-
-    int found = BinarySearch(nums, target, 5);
-    if (found)
-        cout << "PRESENT" << endl;
-    else
-        cout << "ABSENT" << endl;
-
-    return 0;
+    return -1;
 }
