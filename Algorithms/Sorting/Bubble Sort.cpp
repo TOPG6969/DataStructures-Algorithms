a7d3ae8fc1cf37e6f87b3b18dee7a25f6056e3dd
Harshit Shukla <120409631+Harshitshukla0208@users.noreply.github.com>
2023-07-05 21:45:33 +0530

    Create Bubble Sort.cpp

---
+++ Algorithms/Sorting/Bubble Sort.cpp
@@ -0,0 +1,18 @@
+//  ONLY IMPORTANT CODE
+//  Time Complexity = O(n^2)
+//  Space Complexity = O(1)
+
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  for(int i = 1 ; i < arr.size() ; i++)
+  {
+      for(int j = 0 ; j < arr.size()-i ; j++)
+      {
+          if(arr[j] > arr[j+1]) swap(arr[j] , arr[j+1] ;
+      }
+  }
+}
